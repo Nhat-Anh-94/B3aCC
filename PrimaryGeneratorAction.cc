50cde58210b063ba06efced7c27b27afe30e90ec
Nhat-Anh-94 <165351129+Nhat-Anh-94@users.noreply.github.com>
2024-12-27 15:31:38 +0700

    Add files via upload

--- PrimaryGeneratorAction.cc
+++ PrimaryGeneratorAction.cc
@@ -43,7 +43,7 @@ namespace B3
 
 PrimaryGeneratorAction::PrimaryGeneratorAction()
 {
-  G4int n_particle = 10;
+  G4int n_particle = 100;
   fParticleGun = new G4ParticleGun(n_particle);
 
   // default particle kinematic
@@ -51,7 +51,7 @@ PrimaryGeneratorAction::PrimaryGeneratorAction()
   G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
   G4ParticleDefinition* particle = particleTable->FindParticle("gamma");
   fParticleGun->SetParticleDefinition(particle);
-  fParticleGun->SetParticlePosition(G4ThreeVector(0., 0., 0.));
+  fParticleGun->SetParticlePosition(G4ThreeVector(0., 0., -21.));
   fParticleGun->SetParticleEnergy(662 * keV);
   fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0., 0., 1.));
 }
@@ -68,15 +68,15 @@ PrimaryGeneratorAction::~PrimaryGeneratorAction()
 void PrimaryGeneratorAction::GeneratePrimaries(G4Event* event)
 {
 	// Vị trí phát ngẫu nhiên (tuỳ chỉnh vùng không gian nhỏ xung quanh vị trí gốc)
-	G4double x0 = 0 * cm, y0 = 0 * cm, z0 = -20 * cm;
+	G4double x0 = 0 * cm, y0 = 0 * cm, z0 = -21 * cm;
 	G4double dx0 = 1 * cm, dy0 = 1 * cm, dz0 = 1 * cm;
-	x0 += dx0 * (G4UniformRand() - 0.5);
-	y0 += dy0 * (G4UniformRand() - 0.5);
-	z0 += dz0 * (G4UniformRand() - 0.5);
+	x0 += dx0 * (G4UniformRand() - 0.1);
+	y0 += dy0 * (G4UniformRand() - 0.1);
+	z0 += dz0 * (G4UniformRand() - 0.1);
 	fParticleGun->SetParticlePosition(G4ThreeVector(x0, y0, z0));
 
 	// Tạo hướng động lượng ngẫu nhiên trong hình nón
-	G4double coneAngle = 30 * deg;  // Góc hình nón
+	G4double coneAngle = 28 * deg;  // Góc hình nón
 	G4double cosTheta = std::cos(coneAngle);
 	G4double z = cosTheta + (1 - cosTheta) * G4UniformRand();
 	G4double phi = 2 * CLHEP::pi * G4UniformRand();
