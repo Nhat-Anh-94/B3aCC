6791264e4ed726174b095f4bb323c452f1c7076a
Nhat-Anh-94 <165351129+Nhat-Anh-94@users.noreply.github.com>
2025-01-10 11:43:57 +0700

    Add files via upload

--- EventAction.cc
+++ EventAction.cc
@@ -1,84 +1,140 @@
-﻿#include "G4UImanager.hh"
-#include "G4RunManager.hh"
+﻿//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// \file B3/B3a/src/EventAction.cc
+/// \brief Implementation of the B3a::EventAction class
+
+#include "EventAction.hh"
+
+#include "RunAction.hh"
+
+#include "G4Event.hh"
+#include "G4HCofThisEvent.hh"
+#include "G4SDManager.hh"
 #include "G4SystemOfUnits.hh"
-#include <fstream>
-#include <sstream>
+#include "G4THitsMap.hh"
+// #include "G4UnitsTable.hh"
+#include <fstream>  // Thêm thư viện để ghi file
+#include <sstream>  // Để xử lý chuỗi cho tên file
 
 namespace B3a
 {
-    //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-    void EventAction::EndOfEventAction(const G4Event* evt)
-    {
-        // Hits collections
-        G4HCofThisEvent* HCE = evt->GetHCofThisEvent();
-        if (!HCE) return;
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-        // Get hits collections IDs
-        if (fCollID_cryst < 0) {
-            G4SDManager* SDMan = G4SDManager::GetSDMpointer();
-            fCollID_cryst = SDMan->GetCollectionID("detector/edep");
-            fCollID_patient = SDMan->GetCollectionID("patient/dose");
-        }
+EventAction::EventAction(RunAction* runAction) : fRunAction(runAction) {}
 
-        // Energy in crystals : identify 'good events'
-        const G4double eThreshold = 700 * keV;
-        G4int nbOfFired = 0;
-        auto evtMap = static_cast<G4THitsMap<G4double>*>(HCE->GetHC(fCollID_cryst));
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-        for (auto& mapElement : (*evtMap->GetMap())) {
-            auto edep = *(mapElement.second);
-            if (edep > eThreshold) ++nbOfFired;
-        }
-        if (nbOfFired == 2) fRunAction->CountEvent();
+void EventAction::BeginOfEventAction(const G4Event* /*evt*/) {}
 
-        // Dose deposit in patient
-        G4double dose = 0.;
-        evtMap = static_cast<G4THitsMap<G4double>*>(HCE->GetHC(fCollID_patient));
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-        for (auto& mapElement : (*evtMap->GetMap())) {
-            dose += *(mapElement.second);
-        }
-        if (dose > 0.) fRunAction->SumDose(dose);
+void EventAction::EndOfEventAction(const G4Event* evt)
+{
+  // Hits collections
+  //
+  G4HCofThisEvent* HCE = evt->GetHCofThisEvent();
+  if (!HCE) return;
 
-        // Tạo tên file và ghi vào file riêng biệt cho mỗi sự kiện
-        G4int eventID = evt->GetEventID();
-        std::stringstream filename;
-        filename << "event_" << eventID << "_log.txt";  // Tên file: event_<eventID>_log.txt
+  // Get hits collections IDs
+  if (fCollID_cryst < 0) {
+    G4SDManager* SDMan = G4SDManager::GetSDMpointer();
+    fCollID_cryst = SDMan->GetCollectionID("detector/edep");
+    fCollID_patient = SDMan->GetCollectionID("patient/dose");
+  }
 
-        std::ofstream outFile(filename.str());
-        if (outFile.is_open()) {
-            // Ghi thông tin sự kiện vào file
-            outFile << "Sự kiện ID: " << eventID << "\n";
-            outFile << "Số lượng hạt phát ra: " << nbOfFired << "\n";
-            outFile << "Liều lượng tại bệnh nhân: " << dose / gray << " Gy\n";
+  // Energy in crystals : identify 'good events'
+  //
+  const G4double eThreshold = 700 * keV;
+  G4int nbOfFired = 0;
 
-            // Lấy thông tin verbose từ hệ thống Geant4
-            G4UImanager* uiManager = G4UImanager::GetUIpointer();
+  auto evtMap = static_cast<G4THitsMap<G4double>*>(HCE->GetHC(fCollID_cryst));
 
-            // Tạo stream để chuyển hướng G4cout và G4cerr vào file
-            std::streambuf* originalCoutBuf = std::cout.rdbuf();
-            std::ofstream verboseFile("verbose_output_event_" + std::to_string(eventID) + ".txt");
-            std::cout.rdbuf(verboseFile.rdbuf());  // Chuyển hướng G4cout vào file
+  for (auto& mapElement : (*evtMap->GetMap())) {
+    auto edep = *(mapElement.second);
+    if (edep > eThreshold) ++nbOfFired;
+    // auto copyNb  = mapElement.first;
+    // G4cout << "\n  cryst" << copyNb << ": " << edep/keV << " keV ";
+  }
+  if (nbOfFired == 2) fRunAction->CountEvent();
 
-            // Ghi thông tin verbose
-            uiManager->ApplyCommand("/control/verbose 2");  // Đặt mức độ verbose cho control
-            uiManager->ApplyCommand("/tracking/verbose 2"); // Đặt mức độ verbose cho tracking
+  // Dose deposit in patient
+  //
+  G4double dose = 0.;
 
-            // Ghi chi tiết lệnh verbose vào file
-            outFile << "Thông tin từ /control/verbose 2:\n";
-            outFile << "   (Đầu ra sẽ được ghi vào file log này)\n";
+  evtMap = static_cast<G4THitsMap<G4double>*>(HCE->GetHC(fCollID_patient));
 
-            outFile << "Thông tin từ /tracking/verbose 2:\n";
-            outFile << "   (Đầu ra sẽ được ghi vào file log này)\n";
+  for (auto& mapElement : (*evtMap->GetMap())) {
+    dose += *(mapElement.second);
+    // auto copyNb  = mapElement.first;
+    // G4cout << "\n  patient" << copyNb << ": " << G4BestUnit(dose,"Dose");
+  }
+  if (dose > 0.) fRunAction->SumDose(dose);
 
-            // Đảm bảo file được đóng lại sau khi ghi
-            std::cout.rdbuf(originalCoutBuf);  // Quay lại stdout mặc định
-            verboseFile.close();  // Đóng file verbose output
-        }
-        else {
-            G4cerr << "Không thể mở file: " << filename.str() << G4endl;
-        }
-    }
+  // Tạo tên file và ghi vào file riêng biệt cho mỗi sự kiện
+  G4int eventID = evt->GetEventID();
+  std::stringstream filename;
+  filename << "event_" << eventID << "_log.txt";  // Tên file: event_<eventID>_log.txt
+
+  std::ofstream outFile(filename.str());
+  if (outFile.is_open()) {
+      // Ghi thông tin sự kiện vào file
+      outFile << "Sự kiện ID: " << eventID << "\n";
+      outFile << "Số lượng hạt phát ra: " << nbOfFired << "\n";
+      outFile << "Liều lượng tại bệnh nhân: " << dose / gray << " Gy\n";
+
+      // Lấy thông tin verbose từ hệ thống Geant4
+      G4UImanager* uiManager = G4UImanager::GetUIpointer();
+
+      // Tạo stream để chuyển hướng G4cout và G4cerr vào file
+      std::streambuf* originalCoutBuf = std::cout.rdbuf();
+      std::ofstream verboseFile("verbose_output_event_" + std::to_string(eventID) + ".txt");
+      std::cout.rdbuf(verboseFile.rdbuf());  // Chuyển hướng G4cout vào file
+
+      // Ghi thông tin verbose
+      uiManager->ApplyCommand("/control/verbose 2");  // Đặt mức độ verbose cho control
+      uiManager->ApplyCommand("/tracking/verbose 2"); // Đặt mức độ verbose cho tracking
+
+      // Ghi chi tiết lệnh verbose vào file
+      outFile << "Thông tin từ /control/verbose 2:\n";
+      outFile << "   (Đầu ra sẽ được ghi vào file log này)\n";
+
+      outFile << "Thông tin từ /tracking/verbose 2:\n";
+      outFile << "   (Đầu ra sẽ được ghi vào file log này)\n";
+
+      // Đảm bảo file được đóng lại sau khi ghi
+      std::cout.rdbuf(originalCoutBuf);  // Quay lại stdout mặc định
+      verboseFile.close();  // Đóng file verbose output
+  }
+  else {
+      G4cerr << "Không thể mở file: " << filename.str() << G4endl;
+  }
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 }  // namespace B3a
